892457bbca4b87d897c563d572a893bfa6cf3906
theaudaxproject <50760313+theaudaxproject@users.noreply.github.com>
2019-10-05 22:40:57 -0600

    Audaxify8

--- contrib/build-wine/build-secp256k1.sh
+++ contrib/build-wine/build-secp256k1.sh
@@ -4,7 +4,7 @@
 set -e
 
 build_dll() {
-    apt-get install -y mingw-w64
+    sudo apt-get install -y mingw-w64
     export SOURCE_DATE_EPOCH=1530212462
     ./autogen.sh
     echo "LDFLAGS = -no-undefined" >> Makefile.am
